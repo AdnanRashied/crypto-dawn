d7a83206f0bf5e25d86082be868efa43ff5c75ad
CBT1Z <42123280+AdnanRashied@users.noreply.github.com>
2022-05-24 22:18:35 -0400

    isMobile created.

--- sabercrypt/src/components/headers/main_header.component.tsx
+++ sabercrypt/src/components/headers/main_header.component.tsx
@@ -1,36 +1,62 @@
 /*
     main_header.component serves the web application header only.
+    isMobile handles at theme.breakpoints.up-md
 */
 import React from 'react';
 import Stack from "@mui/material/Stack";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import styled from "@mui/material/styles/styled";
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from '@mui/material/styles'
 import { ContainedButton } from "../buttons/main_header_buttons.styles";
-import SaberCircle from "../../assets/SaberCircle.png"
 import "./main_header.styles.css";
 
+import SaberCircle from "../../assets/SaberCircle.png"
+
 const MainHeader = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.up("sm"));
     return (
-        <AppBar>
-            <Toolbar className="toolbar">
-                <img src={SaberCircle}
-                     className="mainHeaderSvg"
-                     alt="mainHeaderSvg"
-                     width={80}
-                />
-                <div className="title">
-                    <h2>SaberCrypt</h2>
-                </div>
-                <div className="button_stack">
-                    <Stack spacing={2} direction="row">
-                        <ContainedButton href="crypto">Crypto</ContainedButton>
-                        <ContainedButton href="market">Market</ContainedButton>
-                    </Stack>
-                </div>
-            </Toolbar>
-        </AppBar>
-    )
-};
+        <div>
+            {isMobile ? (<>
+                <AppBar>
+                    <Toolbar className="toolbar">
+                        <img src={SaberCircle}
+                             className="mainHeaderSvg"
+                             alt="mainHeaderSvg"
+                             width={80}
+                        />
+                        <div className="title">
+                            <h2>SaberCrypt</h2>
+                        </div>
+                        <div className="button_stack">
+                            <Stack spacing={2} direction="row">
+                                <ContainedButton href="crypto">Crypto</ContainedButton>
+                                <ContainedButton href="market">Market</ContainedButton>
+                            </Stack>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+
+            </>
+            ):(
+
+                <AppBar className="appbar_mobile" position={"sticky"}>
+                    <Toolbar className="toolbar_mobile">
+                        <figure className="item">
+                        <img src={SaberCircle}
+                             className="mainHeaderSvg_mobile"
+                             alt="mainHeaderSvg"
+                             width={80}
+                        />
+                        <figcaption className="title_mobile">
+                            <h2>SaberCrypt</h2>
+                        </figcaption>
+                        </figure>
+                    </Toolbar>
+                </AppBar>
+            )}
+        </div>
+)};
 
 export default MainHeader;
